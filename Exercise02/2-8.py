7b7f203115554dff98e0a1be2d5ba9186d161e44
ywyz <admin@ywyz.tech>
2019-06-29 18:55:17 +0800

    Test git

--- Exercise02/2-8.py
+++ Exercise02/2-8.py
@@ -3,3 +3,9 @@
 # @Date    : 2019-06-07 19:01:54
 # @Author  : ywyz (admin@ywyz.tech)
 # @Version : 1.0.0
+# Compute energy
+amountWater = eval(input("Enter the amount of water in kilograms: "))
+intialTemperature = eval(input("Enter the intial temperature: "))
+finalTemperature = eval(input("Enter the final temperature:"))
+needEnergy = amountWater * (finalTemperature - intialTemperature) * 4184
+print("The energy needed is", needEnergy)
