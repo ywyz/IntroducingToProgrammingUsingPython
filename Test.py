0257f568f66d1022d28f5be7272c45210ea3abb4
ywyz <admin@ywyz.tech>
2019-08-24 10:16:25 +0800

    Test

--- Test.py
+++ Test.py
@@ -1,1 +1,10 @@
+'''
+@Date: 2019-08-24 10:16:01
+@Author: ywyz
+@LastModifiedBy: ywyz
+@Github: https://github.com/ywyz
+@LastEditors: ywyz
+@LastEditTime: 2019-08-24 10:16:02
+'''
 print("Hello World!")
+print("Test git!")
