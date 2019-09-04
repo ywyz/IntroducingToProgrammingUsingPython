2026821c3876b30d12f9b50f62424f24ba60aeac
ywyz <admin@ywyz.tech>
2019-09-04 20:49:50 +0800

    Update to Exercise 5-8

--- Exercise05/5-6.py
+++ Exercise05/5-6.py
@@ -1,24 +1,24 @@
 '''
-@Date: 2019-09-02 20:14:41
+@Date: 2019-09-04 20:02:43
 @Author: ywyz
 @LastModifiedBy: ywyz
 @Github: https://github.com/ywyz
 @LastEditors: ywyz
-@LastEditTime: 2019-09-04 20:02:04
+@LastEditTime: 2019-09-04 20:02:54
 '''
 print("------------------------------ ------------------------------")
-print("公斤                   磅     公斤                   磅")
+print("英里                   公里     英里                   公里")
 count = 1
-newcount = 9.09
-while count < 200:
+newcount = 12.430
+while count < 10:
     print(
         count,
         "               ",
-        format(count * 2.2, ".2f"),
+        format(count * 1.609, ".3f"),
         "   |  ",
         format(newcount, ".2f"),
         "               ",
-        format(newcount * 2.2, ".2f"),
+        format(newcount * 1.609, ".2f"),
     )
     count = count + 1
-    newcount += 3.27
+    newcount += 3.108
---
+++ Exercise05/5-8.py
@@ -0,0 +1,16 @@
+'''
+@Date: 2019-09-04 20:48:18
+@Author: ywyz
+@LastModifiedBy: ywyz
+@Github: https://github.com/ywyz
+@LastEditors: ywyz
+@LastEditTime: 2019-09-04 20:49:12
+'''
+import math
+print("------------------------------ ------------------------------")
+print("        Number          |      Square Root          ")
+number = 0
+while number <= 20:
+    print(format(number, "15d"), "       |        ",
+          format(math.sqrt(number), "15.4f"))
+    number += 1
