62f08632167156a6719d8c041c889b5248cfdf6c
ywyz <admin@ywyz.tech>
2019-09-14 19:45:13 +0800

    Update to 5-22

---
+++ Exercise05/5-21.py
@@ -0,0 +1,25 @@
+'''
+@Date: 2019-09-14 17:50:32
+@LastEditors: ywyz
+@LastEditTime: 2019-09-14 19:36:49
+@Author: ywyz
+@Description: Introduction to Programming Using Python
+'''
+
+lineNumber = 8
+nowLine = 0
+for nowLine in range(0, lineNumber):
+    temp = nowLine
+    # 打印空格
+    for temp in range(0, lineNumber - nowLine):
+        print(" ", end="   ")
+
+    # 顺序打印
+    for temp in range(0, nowLine + 1):
+        print(pow(2, temp), end="   ")
+
+    # 倒序打印
+    for temp in range(nowLine - 1, -1, -1):
+        print(pow(2, temp), end="   ")
+
+    print("\n")
---
+++ Exercise05/5-22.py
@@ -0,0 +1,39 @@
+'''
+@Date: 2019-09-14 19:38:52
+@LastEditors: ywyz
+@LastEditTime: 2019-09-14 19:44:24
+@Author: ywyz
+@Description: Introduction to Programming Using Python
+'''
+NUMBER_OF_PRIMES = 50  # Number of primes to display
+NUMBER_OF_PRIMES_PER_LINE = 10  # Display 10 per line
+count = 0  # Count the number of prime numbers
+number = 2  # A number to be tested for primeness
+
+print("The prime numbers between 2 and 1000 are")
+
+# Repeatedly find prime numbers
+while number <= 2000:
+    # Assume the number is prime
+    isPrime = True  # Is the current number prime?
+
+    # Test if number is prime
+    divisor = 2
+    while divisor <= number / 2:
+        if number % divisor == 0:
+            # If true, the number is not prime
+            isPrime = False  # Set isPrime to false
+            break  # Exit the for loop
+        divisor += 1
+
+    # Display the prime number and increase the count
+    if isPrime:
+        count += 1  # Increase the count
+
+        print(format(number, '5d'), end='')
+        if count % NUMBER_OF_PRIMES_PER_LINE == 0:
+            # Display the number and advance to the new line
+            print()  # Jump to the new line
+
+    # Check if the next number is prime
+    number += 1
