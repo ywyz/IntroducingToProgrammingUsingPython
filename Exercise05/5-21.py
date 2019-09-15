09e960f8cddc7d40e390e43e05444e3701f70369
ywyz <admin@ywyz.tech>
2019-09-15 20:07:31 +0800

    start:#1 修复 5-3～5-8 5-12 5-13 5-21 格式

--- Exercise05/5-21.py
+++ Exercise05/5-21.py
@@ -1,7 +1,7 @@
 '''
 @Date: 2019-09-14 17:50:32
 @LastEditors: ywyz
-@LastEditTime: 2019-09-15 20:01:55
+@LastEditTime: 2019-09-15 20:07:13
 @Author: ywyz
 @Description: Introduction to Programming Using Python
 '''
@@ -16,10 +16,10 @@ for nowLine in range(0, lineNumber):
 
     # 顺序打印
     for temp in range(0, nowLine + 1):
-        print(pow(2, temp), end="     ")
+        print(pow(2, temp), end="    ")
 
     # 倒序打印
     for temp in range(nowLine - 1, -1, -1):
-        print(pow(2, temp), end="     ")
+        print(pow(2, temp), end="    ")
 
     print("\n")
