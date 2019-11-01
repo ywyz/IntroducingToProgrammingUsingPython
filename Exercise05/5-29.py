69b5471432fab39684345bb40bb0aeea64583a88
ywyz <admin@ywyz.tech>
2019-11-01 01:44:36 -0400

    Fixed 5-29.py

--- Exercise05/5-29.py
+++ Exercise05/5-29.py
@@ -1,5 +1,24 @@
-for n in range(2001, 2101):
-    for number in range(0, 10):
-        if n % 100 == 0:
-        if
-
+n = 2001
+number = 1
+while n < 2101:
+    if number % 10 == 0:
+        print("\n")
+        number += 1
+    if n % 100 == 0:
+        if n % 400 == 0:
+            print(n, end=" ")
+            n += 1
+            number += 1
+            continue
+        else:
+            n += 1
+            continue
+    else:
+        if n % 4 == 0:
+            print(n, end=" ")
+            n += 1
+            number += 1
+            continue
+        else:
+            n += 1
+            continue
