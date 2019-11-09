e2657d290ae5d3f0339fb1a2109871e85559f215
ywyz <admin@ywyz.tech>
2019-11-09 23:59:04 +0800

    fix 5-8.py

--- Exercise05/5-4.py
+++ Exercise05/5-4.py
@@ -4,11 +4,14 @@
 @LastModifiedBy: ywyz
 @Github: https://github.com/ywyz
 @LastEditors: ywyz
-@LastEditTime: 2019-09-02 20:04:44
+@LastEditTime: 2019-11-09 23:58:47
 '''
 print("------------------------------")
 print("英里                   公里")
 count = 1
 while count < 11:
-    print(count, "               ", format(count * 2.2, "4f"))
+    if count >= 10:
+        print(count, "              ", format(count * 2.2, "4f"))
+    else:
+        print(count, "               ", format(count * 2.2, "4f"))
     count = count + 1
