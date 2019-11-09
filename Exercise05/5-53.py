07d2978e4fe4e9ad4c0a543700e9eca4e3cf469a
ywyz <admin@ywyz.tech>
2019-11-09 23:09:39 +0800

    Updated to 5-53.py

--- Exercise05/5-53.py
+++ Exercise05/5-53.py
@@ -1,10 +1,10 @@
 '''
-@Date: 2019-11-09 22:55:31
+@Date: 2019-11-09 23:04:06
 @Author: ywyz
 @LastModifiedBy: ywyz
 @Github: https://github.com/ywyz
 @LastEditors: ywyz
-@LastEditTime: 2019-11-09 23:08:55
+@LastEditTime: 2019-11-09 23:08:04
 '''
 import turtle
 import math
@@ -33,13 +33,22 @@ turtle.goto(0, 200)
 turtle.goto(15, 175)
 turtle.penup()
 
+# Sin函数
+turtle.color("blue")
 for x in range(-175, 176):
     turtle.goto(x, 50 * math.sin((x / 100) * 2 * math.pi))
+    turtle.pendown()
+# Cos函数
+turtle.penup()
+turtle.color("red")
+for x in range(-175, 176):
+    turtle.goto(x, 50 * math.cos((x / 100) * 2 * math.pi))
     turtle.pendown()
 
 turtle.penup()
 turtle.goto(-100, -15)
 turtle.pendown()
+turtle.color("black")
 turtle.write("-2\u03c0")
 turtle.penup()
 turtle.goto(100, -15)
