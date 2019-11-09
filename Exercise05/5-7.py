f16c330eb9b02fc201a0ef45d57ae3e205bcd434
ywyz <admin@ywyz.tech>
2019-11-10 00:06:39 +0800

    fix 5-7.py

--- Exercise05/5-7.py
+++ Exercise05/5-7.py
@@ -4,15 +4,28 @@
 @LastModifiedBy: ywyz
 @Github: https://github.com/ywyz
 @LastEditors: ywyz
-@LastEditTime: 2019-09-04 20:24:48
+@LastEditTime: 2019-11-10 00:06:07
 '''
 import math
 print("------------------------------ ------------------------------")
-print("        degree     |       sin         |          cos        |")
+print(
+    "        degree    |              sin                |          cos        "
+)
 degrees = 0
 while degrees <= 360:
-    print("        ", degrees, "     |       ",
-          format(math.sin(math.radians(degrees)), "15.4f"),
-          "         |          ",
-          format(math.cos(math.radians(degrees)), "15.4f"), "       ")
+    if degrees <= 90 and degrees > 0:
+        print("         ", degrees, "     |       ",
+              format(math.sin(math.radians(degrees)), "15.4f"),
+              "         |          ",
+              format(math.cos(math.radians(degrees)), "15.4f"), "       ")
+    elif degrees == 0:
+        print("          ", degrees, "     |       ",
+              format(math.sin(math.radians(degrees)), "15.4f"),
+              "         |          ",
+              format(math.cos(math.radians(degrees)), "15.4f"), "       ")
+    else:
+        print("        ", degrees, "     |       ",
+              format(math.sin(math.radians(degrees)), "15.4f"),
+              "         |          ",
+              format(math.cos(math.radians(degrees)), "15.4f"), "       ")
     degrees += 10
