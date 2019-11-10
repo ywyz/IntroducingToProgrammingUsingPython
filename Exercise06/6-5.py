7a238ba86d3c6cdd8cb70be3083958f47e2f5d9a
ywyz <admin@ywyz.tech>
2019-11-10 19:31:53 +0800

    updated to 6-5.py

---
+++ Exercise06/6-5.py
@@ -0,0 +1,27 @@
+'''
+@Date: 2019-11-10 19:12:57
+@Author: ywyz
+@LastModifiedBy: ywyz
+@Github: https://github.com/ywyz
+@LastEditors: ywyz
+@LastEditTime: 2019-11-10 19:30:59
+'''
+
+
+def displaySortedNumbers(num1, num2, num3):
+    if num1 > num2:
+        num1, num2 = num2, num1
+    if num2 > num3:
+        num2, num3 = num3, num2
+    if num1 > num2:
+        num1, num2 = num2, num1
+
+    return num1, num2, num3
+
+
+def main():
+    n1, n2, n3 = eval(input("Enter three number:"))
+    print(displaySortedNumbers(n1, n2, n3))
+
+
+main()
