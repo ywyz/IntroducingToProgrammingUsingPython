97542efd7435306003e243585b41d830bfdcc55c
ywyz <admin@ywyz.tech>
2019-11-10 20:12:33 +0800

    Updatecd /home/ywyz/code/IntroducingToProgrammingUsingPython ; env PYTHONIOENCODING=UTF-8 PYTHONUNBUFFERED=1 /usr/bin/python3 /home/ywyz/.vscode/extensions/ms-python.python-2019.10.44104/pythonFiles/ptvsd_launcher.py --default --client --host localhost --port 34117 /home/ywyz/code/IntroducingToProgrammingUsingPython/Exercise06/6-7.py

---
+++ Exercise06/6-7.py
@@ -0,0 +1,28 @@
+'''
+@Date: 2019-11-10 19:56:39
+@Author: ywyz
+@LastModifiedBy: ywyz
+@Github: https://github.com/ywyz
+@LastEditors: ywyz
+@LastEditTime: 2019-11-10 20:11:42
+'''
+
+
+def futureInvestmentValue(investAmount, monthlyInterestRate, years):
+    total = investAmount * (1 + monthlyInterestRate)**(years * 12)
+    return total
+
+
+def main():
+    amountInvest = eval(input("The amoubt invested: "))
+    annualIntersetRate = eval(input("Annual interest rate: "))
+    print("Years           Future Value")
+    for year in range(1, 31):
+        print(
+            format(year, "6d"),
+            format(
+                futureInvestmentValue(amountInvest, annualIntersetRate / 1200,
+                                      year), "15.2f"))
+
+
+main()
